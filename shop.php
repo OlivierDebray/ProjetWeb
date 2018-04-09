e06622e380cb2505c36666217e51fc1b65ca928a
TheoRobineau <theo.robineau@viacesi.fr>
2018-04-09 14:15:50 +0200

    Page Shop

---
+++ shop.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+<?php include('includes/head.php') ?>
+
+<?php include('includes/mainNavbar.php') ?>
+<?php include ('includes/shopNavbar.php') ?>
+
+<?php include('includes/footer.php') ?>
+</html>
+
