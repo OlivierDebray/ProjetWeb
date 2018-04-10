41b445bfac348ed2d091a3741d5efb0a1933080d
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2018-04-10 15:11:27 +0200

    insertion css

--- includes/head.php
+++ includes/head.php
@@ -3,4 +3,5 @@
     <meta charset="utf-8">
     <link rel="icon" type="image/png" href="images/favicon.png" />
     <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/panier.css" />
 </head>
