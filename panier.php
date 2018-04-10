f9271e4b5e7be84ec447b3f25816a9cbfb5b9b5a
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2018-04-10 14:54:26 +0200

    test

--- panier.php
+++ panier.php
@@ -7,7 +7,14 @@
     <?php include('includes/mainNavbar.php') ?>
     <?php include ('includes/shopNavbar.php') ?>
     <h1>Voici votre panier !</h1>
-
+<div class="table">
+    	<div class="rowtitle">
+    		<span class= "name"> NomProduit</span>
+    		<span class="price"> Prix</span>
+    		<span class="Quantity"> Quantite</span>
+    		<span class="action"> Supprimer</span>
+    	</div>
+    </div>
 </body>
 
     <?php include('includes/footer.php') ?>
