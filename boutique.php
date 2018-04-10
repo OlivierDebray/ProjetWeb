5d458805615aa8d2016291558fc8b9ae753a9ba9
TheoRobineau <theo.robineau@viacesi.fr>
2018-04-10 20:21:36 +0200

    Addes Css boutique

--- boutique.php
+++ boutique.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 
 <html>
+    <head>
     <?php include('includes/head.php') ?>
-
+    <link rel="stylesheet" type="text/css" href="css/boutique.css"/>
+    </head>
 <body>
     <?php include ('includes/header.php')?>
     <?php include('includes/mainNavbar.php') ?>
     <?php include ('includes/shopNavbar.php') ?>
-    <h1>Bienvenue sur la Boutique du BDE !</h1>
-    <div class="products"></
+    <h1 id="corpus">Bienvenue sur la Boutique du BDE !</h1>
+    <div id="corpus" class="products"></
     <p> <?php include ('Scripts/DisplayProduct.php') ?></p>
 
     </div>
