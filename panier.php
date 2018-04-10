913cf8916f9e4b608f67d747465e23b9193ee351
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2018-04-10 14:22:37 +0200

    panier

---
+++ panier.php
@@ -0,0 +1,14 @@
+<!DOCTYPE.html>
+<html>
+    <?php include('includes/head.php') ?>
+
+<body>
+    <?php include ('includes/header.php')?>
+    <?php include('includes/mainNavbar.php') ?>
+    <?php include ('includes/shopNavbar.php') ?>
+    <h1>Voici votre panier !</h1>
+
+</body>
+
+    <?php include('includes/footer.php') ?>
+</html>
