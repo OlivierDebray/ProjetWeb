59e753ea899618761584020eb1b5d3c36eedb911
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2018-04-11 16:08:57 +0200

    panier

--- addpanier.php
+++ addpanier.php
@@ -1,9 +1,46 @@
 <?php
+session_start();
+try{
+	$bdd = new PDO('mysql:host=localhost;dbname=projetweb', 'root', '');
+}
 
-$bdd = new PDO('mysql:host=localhost;dbname=projetweb', 'root', '');
+catch(Exception $e){
 
-$id= $_SESSION["id"];
+	die('Erreur:' . $e->getmessage());
+}
 
-$reqpanier = $bdd->prepare('SELECT * FROM panier WHERE ID_Utilisateur = $id')
-$reqpanier->execute();
+
+if(isset($_SESSION["id"])) {
+	$id= $_SESSION["id"];
+
+		$reqpanier = $bdd->prepare('SELECT * FROM panier WHERE ID_Utilisateur = $id');
+		$reqpanier->execute();
+
+	while($donnée = $reqpanier->fletch()){ ?>
+		<div class="table">
+			<div class="wrap">
+				<div class="rowtitle">
+					<span class="image"> </span>
+					<span class= "name"> NomProduit</span>
+					<span class="price"> Prix</span>
+					<span class="Quantity"> Quantite</span>
+					<span class="action"> Supprimer</span>
+				</div>
+
+				<div class= "row">
+					<img class="img" src="images/BergerAustralien.jpg" alt="chien" > 
+					<span class= "name"> Berger Australien</span>
+					<span class="price"> 1000 €</span>
+					<span class="Quantity"> 1</span>
+					<span class="action"> Supprimer</span>
+				</div>
+			</div>   
+		</div> 
+		<?php
+	}
+}
+else {
+	
+}
+
 ?>
--- panier.php
+++ panier.php
@@ -8,25 +8,8 @@
     <?php include ('includes/shopNavbar.php') ?>
     <section id="corpus">
         <h1>Voici votre panier !</h1>
-        <div class="table">
-            <div class="wrap">
-            	<div class="rowtitle">
-                    <span class="image"> </span>
-            		<span class= "name"> NomProduit</span>
-            		<span class="price"> Prix</span>
-            		<span class="Quantity"> Quantite</span>
-            		<span class="action"> Supprimer</span>
-            	</div>
-
-                <div class= "row">
-                    <img class="img" src="images/BergerAustralien.jpg" alt="chien" > 
-                    <span class= "name"> Berger Australien</span>
-                    <span class="price"> 1000 €</span>
-                    <span class="Quantity"> 1</span>
-                    <span class="action"> Supprimer</span>
-                </div>
-             </div>   
-        </div>  
+<p> <?php include('addpanier.php') ?></p>
+ 
     </section>
 </body>
 
