8b9b21bd909dde4387f9fd252868e565fffa0db7
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2018-04-16 15:09:50 +0200

    Demande RDV
    Mise en place des demandes de rendez-vous.

--- ValiderCommande.php
+++ ValiderCommande.php
@@ -1,4 +1,4 @@
-<?php session_start() ?>
+<?php session_start()?>
 
 <!DOCTYPE html>
 <html>
@@ -12,9 +12,48 @@
     <?php include('includes/mainNavbar.php') ?>
     <?php include ('includes/shopNavbar.php') ?>
     <section id="corpus">
-        <h1>Veuillez choisir votre mode de paiment</h1>
-<button><a href="DemandeRDV.php"> Demander un rendez-vous</a></button>    
-<button><a href="Paypal.php"> Paypal</a></button>    
+        <h1>Veuillez choisir votre mode de paiment</h1>    
+<button><a href="Paypal.php"> Paypal</a></button>  
+
+<?php
+
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+
+catch(Exception $e){
+
+    die('Erreur:' . $e->getmessage());
+}
+
+
+if(isset($_SESSION["id"])) {
+    $id= $_SESSION["id"];
+
+    $client = $bdd->query("SELECT * FROM utilisateurs WHERE ID_Utilisateurs=".$id)->fetch();
+
+    $reqpersonne= $bdd->prepare("SELECT * FROM utilisateurs WHERE Status = '1'");
+    $reqpersonne->execute();
+
+    while ($donnée = $reqpersonne->fetch()){
+
+        $message = $client['Nom']." ".$client['Prenom']." a commandé sur le site du BDE. Veuillez lui donner un rendez-vous! ";
+
+        $bdd->query("INSERT INTO notifications (FK_ID_Utilisateur, Message) VALUES ( ".$donnée['ID_Utilisateurs'].",'".addslashes($message)."')");
+
+    }
+
+
+}
+
+else {
+    die("Vous n'êtes pas connecté. Veuillez vous connecter à votre compte pour accéder à votre panier");
+}
+
+
+?>
+
 
 </section>
 </body>
