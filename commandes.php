30eb12a2d913b96ccc1f0cfffc325eb521c117cb
OlivierDebray <olivier.debray@viacesi.fr>
2018-04-18 13:20:32 +0200

    Corrections

--- commandes.php
+++ commandes.php
@@ -21,26 +21,26 @@ if (isset($_GET['id'])) {
 <?php include('includes/shopNavbar.php') ?>
 
 <section id="corpus">
-    <?php if (isset($_SESSION['id']) AND $_SESSION['etat'] == 1) {
-    $bdd = new PDO('mysql:host=127.0.0.1;dbname=projetweb;charset=utf8', 'root', ''); ?>
-    <h1>Affichage des commandes en cours</h1>
-    <?php
-    $commandeCours = $bdd->query("SELECT * FROM commande WHERE Etat=0");
-    while ($commande = $commandeCours->fetch()) {
-        $user = $bdd->query("SELECT * FROM utilisateurs WHERE ID_Utilisateurs=".$commande['Utilisateur'])->fetch();
-        $product = $bdd->query("SELECT * FROM produits WHERE ID_Produits=".$commande['Produit'])->fetch();
-        echo "<p id='para".$commande['ID_Commande']."'>";
-        echo $commande['Date']." | ";
-        echo $user['Nom']." ".$user['Prenom']." | ";
-        echo $product['Nom']." x ".$commande['Quantite']." ";
-        echo "<button id='button".$commande['ID_Commande']."' onclick='changeCommandeState(".$commande['ID_Commande'].")'>Marquer cette commande comme livrée</button>";
-        echo "</p>";
-    }
-    ?>
-</section>
-<section id="corpus">
-    <h1>Affichage des commandes livrées</h1>
-    <section id="commandesLivrees">
+    <div id="commandesEnCours">
+        <?php if (isset($_SESSION['id']) AND $_SESSION['etat'] == 1) {
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=projetweb;charset=utf8', 'root', ''); ?>
+        <h1>Affichage des commandes en cours</h1>
+        <?php
+        $commandeCours = $bdd->query("SELECT * FROM commande WHERE Etat=0");
+        while ($commande = $commandeCours->fetch()) {
+            $user = $bdd->query("SELECT * FROM utilisateurs WHERE ID_Utilisateurs=".$commande['Utilisateur'])->fetch();
+            $product = $bdd->query("SELECT * FROM produits WHERE ID_Produits=".$commande['Produit'])->fetch();
+            echo "<p id='para".$commande['ID_Commande']."'>";
+            echo $commande['Date']." | ";
+            echo $user['Nom']." ".$user['Prenom']." | ";
+            echo $product['Nom']." x ".$commande['Quantite']." ";
+            echo "<button id='button".$commande['ID_Commande']."' onclick='changeCommandeState(".$commande['ID_Commande'].")'>Marquer cette commande comme livrée</button>";
+            echo "</p>";
+        }
+        ?>
+    </div>
+    <div id="commandesLivrees">
+        <h1>Affichage des commandes livrées</h1>
         <?php
         $commandeCours = $bdd->query("SELECT * FROM commande WHERE Etat=1");
         while ($commande = $commandeCours->fetch()) {
@@ -55,7 +55,7 @@ if (isset($_GET['id'])) {
         } else {
             echo "Vous devez être connecté et membre du BDE pour voir cette page !";
         }?>
-    </section>
+    </div>
 </section>
 
 <?php include('includes/footer.php') ?>
--- includes/shopNavbar.php
+++ includes/shopNavbar.php
@@ -1,6 +1,6 @@
 <nav id="secondary">
     <?php if (isset($_SESSION['etat']) AND ($_SESSION['etat'] == 1)) { ?>
-        <a href="ajoutProduit.php">Ajouter un produit</a>
+        <a href="ajoutProduits.php">Ajouter un produit</a>
         <a href="commandes.php">Commandes</a>
     <?php } ?>
 </nav>
