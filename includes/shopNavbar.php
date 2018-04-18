e7ae7949aeb184fe86b28d48ad4b1a372f3f7881
OlivierDebray <olivier.debray@viacesi.fr>
2018-04-18 11:09:56 +0200

    Ajout de l'affichage des commandes

--- includes/shopNavbar.php
+++ includes/shopNavbar.php
@@ -1,3 +1,6 @@
-<nav>
-
+<nav id="secondary">
+    <?php if (isset($_SESSION['etat']) AND ($_SESSION['etat'] == 1)) { ?>
+        <a href="ajoutProduit.php">Ajouter un produit</a>
+        <a href="commandes.php">Commandes</a>
+    <?php } ?>
 </nav>
