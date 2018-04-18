8dd42faae9f33c5c1c6331fd7e8c0a051c985154
TheoRobineau <theo.robineau@viacesi.fr>
2018-04-18 16:35:50 +0200

    Navbar

--- includes/shopNavbar.php
+++ includes/shopNavbar.php
@@ -2,4 +2,7 @@
     <a href="categorie.php">Categories</a>
     <a href="recherche.php">Recherche</a>
     <a href="panier.php"> Panier</a>
+    <?php if (isset($_SESSION['etat']) AND ($_SESSION['etat'] == 1)) { ?>
+        <a href="ajoutProduits.php">Ajouter un produit</a>
+    <?php } ?>
 </nav>
