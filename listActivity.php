1d4451f9e7e7690e7a78e31190175f0d54c1606d
Nex45 <patricia.hu@viacesi.fr>
2018-04-19 10:28:07 +0200

    listActivity - Comment
    Commentaires de listActivity.php

--- listActivity.php
+++ listActivity.php
@@ -1,10 +1,10 @@
 <?php
+    /*
+     * On récupère toutes les données de la table évènements
+     */
     $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
-
     $requete = $bdd->prepare("SELECT * FROM evenements");
-
     $requete->execute();
-
     $activities = $requete -> fetchAll();
 
 ?>
