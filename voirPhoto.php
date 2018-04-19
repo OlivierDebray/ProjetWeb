b0eb24fd5e6d099234bafda9eada11a83a628a1b
OlivierDebray <olivier.debray@viacesi.fr>
2018-04-19 11:01:36 +0200

    Ajout des options d'administrations des éléments

--- voirPhoto.php
+++ voirPhoto.php
@@ -28,6 +28,13 @@
             <div class='product'>
                 <img src='images/photosEvenement/<?php echo $_GET['numEvent']?>/<?php echo $reponse['url']?>' class='imgprod' alt="image" />
                 <button onclick="window.location.assign('downloadPicture.php?numImage=<?php echo $reponse['ID_Image']?>')">Telecharger</button>
+                <?php if ($_SESSION['etat'] == 1) {
+                    echo "<button onclick='supprimerImage(\"\",".$reponse['ID_Image'].")'>Supprimer la photo</button>";
+                }
+                else if ($_SESSION['etat'] == 2) {
+                    echo "<button onclick='supprimerImage(\"".$_SESSION['nom']." ".$_SESSION['prenom']."\",".$reponse['ID_Image'].")'>Supprimer la photo et notifier le BDE</button>";
+                }
+                ?>
             </div>
             <div class="userAction">
                 <?php $likeReq = $bdd->prepare("SELECT COUNT(*) FROM `action` WHERE Image=? AND `Like`=1");
